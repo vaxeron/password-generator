05b7d8596f2e75640f5cc7386e8c4a8a3e721f17
vaxeron <79334240+vaxeron@users.noreply.github.com>
2022-08-03 15:01:23 +0300

    Add files via upload

--- password generator.bat
+++ password generator.bat
@@ -1,37 +1,16 @@
-@echo off
-color 0a
-title Random password generator
-echo random password generator
-:Start
-cls
-echo.
-echo 1 for Random Password
-echo.
-set /p input= Enter 1 to generate random password: 
-if %input%==1 goto A if not goto Start
+@echo off & setlocal EnableDelayedExpansion
+color c
+set "alpha=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+set alphaCnt=70
 
-:A
-cls
-Setlocal EnableDelayedExpansion
-set _RNDLength=12
-set _Alphanumeric=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-set _Str=%_Alphanumeric%987654321
+For /L %%j in (1,1,10) DO CALL :GEN %%j
 
-:_LenLoop
-if not "%_Str:~18%"=="" set _Str=%_Str:~9%& set /A _Len+=9& goto :_LenLoop
-set _tmp=%_Str:~9,1%
-set /A _Len=_Len+_tmp
-set _count=0
-set _RndAlphaNum=
-
-:_loop
-set /a _count+=1
-set _RND=%Random%
-set /A _RND=_RND%%%_Len%
-set _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
-if !_count! lss %_RNDLength% goto _loop
-echo.
-echo Password i created: !_RndAlphaNum!
-echo.
-pause > nul
-exit
+pause
+Goto :Eof
+:GEN
+Set "Password="
+For /L %%j in (1,1,10) DO (
+    Set /a i=!random! %% alphaCnt
+    Call Set PASSWORD=!PASSWORD!%%alpha:~!i!,1%%
+)
+echo Your Random Password %1 is [%PASSWORD%]
